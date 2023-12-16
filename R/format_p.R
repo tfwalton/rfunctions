a330d1aaedff4d66cc84b8089f995c02240d3182
tfwalton <tomwaltonaes@gmail.com>
2023-12-16 10:28:38 +0800

    added function for formatting p-values

---
+++ R/format_p.R
@@ -0,0 +1,23 @@
+#' Format p-values
+#'
+#' This function will print p-values according to APA-7 guidelines
+#' @param no arguments necessary
+#' @keywords p-value
+#' @export
+#' @examples
+#' values <- c(0.2, 0.035, 0.00021)
+#' vapply(values, format_p, character(1))
+
+
+# format p-values
+format_p <- function(x){
+  if (is.na(x)) {
+    print("")
+  } else if (x < 0.001) {
+    print("p < .001")
+  } else {
+    stringr::str_remove((round(x, digits = 3)), "^0+")
+  }
+}
+
+

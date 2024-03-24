eb2f2fd7406e1df0bb72f23d56fd2d5dae05d823
tfwalton <tomwaltonaes@gmail.com>
2024-03-24 11:29:28 +0800

    Added capability to suppress "p" in p-value. Added capability to apply to objects with two dimensions.

--- R/format_p.R
+++ R/format_p.R
@@ -8,15 +8,24 @@
 #' values <- c(0.2, 0.035, 0.00021)
 #' vapply(values, format_p, character(1))
 
+format_p <- function(x, suppress.p = FALSE) {
+  if ((ncol(as.data.frame(x)) > 1 & nrow(as.data.frame(x)) > 1) == TRUE) {
+    apply(x, c(1,2), format1, suppress.p = suppress.p)
+  } else {sapply(x, format1, suppress.p = suppress.p)}
+}
 
 # format p-values
-format_p <- function(x){
+format1 <- function(x, suppress.p = FALSE){
   if (is.na(x)) {
     ("")
-  } else if (x < 0.001) {
-    ("p < .001")
-  } else {
-    stringr::str_remove((round(x, digits = 3)), "^0+")
+  } else if (as.numeric(x) < 0.001) {
+      if(suppress.p == FALSE) {
+        ("p < .001")
+      } else {
+        ("< .001")
+      }
+    } else {
+    stringr::str_remove((round(as.numeric(x), digits = 3)), "^0+")
   }
 }
 

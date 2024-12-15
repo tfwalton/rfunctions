12be465ef1fc482948523fa01d2be9656a7db6d3
tfwalton <tomwaltonaes@gmail.com>
2024-12-15 19:42:03 +0800

    Added function to format numbers with no leading zero (e.g., for reporting correlations)

---
+++ R/format_nozero.R
@@ -0,0 +1,32 @@
+#' Format numbers
+#'
+#' This function will round numbers according to APA7 guidelines with no leading zero
+#' @param No arguments necessary
+#' @keywords round
+#' @export
+#' @examples
+#' format_num(0.053634)
+
+
+format_nozero <- function(x) {
+  if (dim(as.data.frame(x))[1] > 1 & dim(as.data.frame(x))[2] > 1) {
+    apply(x, c(1,2), format1)
+  } else {sapply(x, format1)}
+}
+
+# express numbers in scientific notation or round where necessary
+
+
+format2 <- function(x) {
+  if (is.na(x)) {
+    ""
+  } else if(x == 0) {
+    round(x, digits = 0)
+  } else if (abs(x) < 0.001) {
+    formatC(x, format = "e", digits = 0)
+  }  else if (abs(x) < 1) {
+    substr(round(x, digits = 3),2,nchar(round(x, digits = 3)))
+  } else {
+    stop("Error: input must be a number < 1")
+  }
+}
